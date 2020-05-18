a5284e38400a70e777b3b8ab96ba235005a031ad
LMField82 <meneslindsay@hotmail.com>
2020-05-18 14:54:14 -0600

    making sequelize connection and comments

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
+//Dependencies
 const express = require("express");
 const mysql = require("mysql");
+
+//Setting up express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Requiring our models for syncing
+const db = require("./models");
+
+//Setting up express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Static Directories
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }else {
   app.use(express.static("client/public"))
 }
 
+//Setting up authentication
 // const passport = require('./config/passport');
-
 // app.use(passport.initialize())
 // app.use(require('./routes'))
 
-connection.connect(err => {
+//Routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+//Syncing sequelize models and starting express app
+db.Sequelize.sync({ force: true }).then(() => {
+  connection.connect(err => {
     (err) ? console.log("error", err) : console.log("connection");
-});
-
-app.listen(PORT, function () {
-
+  });
+  app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!!!`);
+  });
 
 });
