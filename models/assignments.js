03adce997ff4e53e5f4aa64aec914d1ec52f1090
LMField82 <meneslindsay@hotmail.com>
2020-05-18 11:48:30 -0600

    assignment model set up

---
+++ models/assignments.js
@@ -0,0 +1,18 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const Assignment = sequelize.define('Assignment', {
+    assignment_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true
+    },
+    standard: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    //other table options
+});
+
+console.log(Assignment === sequelize.models.Assignment);
